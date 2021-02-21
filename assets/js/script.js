92cb3969a71923a9785ec4154b04e49037bf3dfd
Jason Jones <jason.e.jones@gmail.com>
2021-02-20 20:58:00 -0500

    checkpoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,22 @@ var startPage = document.getElementById("start-page");
 var gamePage = document.getElementById("game-page");
 var questionsPage = document.getElementById("questions-page");
 // var buttonAnswerEl = document.getElementById("answer-button");
-var buttonDiv = document.getElementById("button-ol");
-startBtn.addEventListener("click", function () {
-  startPage.style.display = "none";
-  questionCard.style.display = "flex";
-  // document.getElementById("start-page").style.display = "none";
-});
+var questionsText = document.getElementById("questions-text");
 
+var buttonDiv = document.getElementById("button-ol");
+var counterEl = document.getElementById("counter");
 var QuizTable = [];
+var correctAnswer = "";
 var IndividualQuestion = [];
 var questionIndex = 0;
 var questionCard = document.querySelector("#question-card");
 var btnCreate = document.createElement("button");
 var buttonolEl = document.querySelector("#button-ol");
+var secondsLeft = 10;
+var quizOn = false;
+console.log (questionIndex+" "+quizOn);
+
+
 
 
 
@@ -28,30 +31,56 @@ var questions =
       correctAnswer: "Answer 1.3"
     },
 
-    // {
-    //   question: "Question 2",
-    //   choices: ["Answer 2.1", "Answer 2.2", "Answer 2.3"],
-    //   correctAnswer: 3
-    // },
+    {
+      question: "Question 2",
+      choices: ["Answer 2.1", "Answer 2.2", "Answer 2.3"],
+      correctAnswer: "Answer 2.3"
+    },
 
-    // {
-    //   question: "Question 3",
-    //   choices: ["Answer 3.1", "Answer 3.2", "Answer 3.3", "Answer 3.4"],
-    //   correctAnswer: 2
-    // },
+    {
+      question: "Question 3",
+      choices: ["Answer 3.1", "Answer 3.2", "Answer 3.3", "Answer 3.4"],
+      correctAnswer: "Answer 3.2"
+    },
 
-    // {
-    //   question: "Question 4",
-    //   choices: ["Answer 4.1", "Answer 4.2", "Answer 4.3", "Answer 4.4"],
-    //   correctAnswer: 2
-    // },
+    {
+      question: "Question 4",
+      choices: ["Answer 4.1", "Answer 4.2", "Answer 4.3", "Answer 4.4"],
+      correctAnswer: "Answer 4.1"
+    },
 
-    // {
-    //   question: "Question 5",
-    //   choices: ["Answer 5.1", "Answer 5.2", "Answer 5.3", "Answer 5.4"],
-    //   correctAnswer: 2
-    // }
+    {
+      question: "Question 5",
+      choices: ["Answer 5.1", "Answer 5.2", "Answer 5.3", "Answer 5.4"],
+      correctAnswer: "Answer 5.4"
+    }
   ];
+
+
+startBtn.addEventListener("click", function () {
+  startPage.style.display = "none";
+  questionCard.style.display = "flex";
+  quizOn = true;
+  console.log ("wtf "+" "+quizOn);
+  countdownTimer();
+  playOn();
+  // document.getElementById("start-page").style.display = "none";
+});
+
+function playOn () {
+  // for (questionIndex = 0; questionIndex < questions.length; questionIndex++) {
+  // console.log ("im here "+questionIndex+" "+quizOn);
+  //   if (quizOn == true) {
+  //     showQuestion (questions[questionIndex]);
+  //     console.log ("ive returned from showQuestion");
+  //   }
+  // }
+  showQuestion (questions[0]);
+  showQuestion (questions[1]);
+}
+
+// showQuestion (questions[1]);
+
 
 // for (var i = 0; i < questions.length; i++) {
 //     currentquestion = questions[i].question;
@@ -66,86 +95,71 @@ var questions =
 
 // for (var i = 0; i < questions.length; i++) {
 // }
-for (var j = 0; j < questions[questionIndex].choices.length; j++) {
-  
-  // console.log (questions[questionIndex].choices[j]);
-  var buttonAnswerEl = `<button class='button answer-btn'>${questions[questionIndex].choices[j]}</button>`;
-  // buttonolEl.innerHTML += buttonAnswerEl;
-  buttonolEl.innerHTML += buttonAnswerEl;
-  buttonolEl.onclick = helloworld;
-  
 
+// function showQuestion (lquestion) {
+//   correctAnswer = questions[questionIndex].correctAnswer;
+//    for (var j = 0; j < questions[questionIndex].choices.length; j++) {
+ 
+//      if (questions[questionIndex].choices[j] === questions[questionIndex]) {
+//        var answer=true;
+//        } else {
+//        var answer=false;
+//      }
+//      var buttonAnswerEl = `<button class='button answer-btn' data-isCorrect=${answer}>${questions[questionIndex].choices[j]}</button>`;
+//      buttonolEl.innerHTML += buttonAnswerEl;
+//      buttonolEl.onclick = answerCheck; 
+//    }
+//  }
 
-  // var myBtn = document.createElement("button");
-  // myBtn.textContent = "here";
-  // myBtn.document.createElement("button");
-  // var buttonAnswerEl = (`<button class='button answer-btn'></button>`);
-  // buttonDiv.append(buttonAnswerEl);
+function showQuestion (lquestion) {
+quizOn=false;
+ displayQuestion=false; // need to hold up until we get the click action
+ questionsText.textContent = lquestion.question;  // push out the text of the question
+ correctAnswer = lquestion.correctAnswer; // lets pull the correctAnswer out and hold it here
+ console.log ("correntAnswer is "+correctAnswer);
+  for (var j = 0; j < lquestion.choices.length; j++) {  //let loop through number of answers
+
+    var buttonAnswerEl = `<button class='button answer-btn'>${lquestion.choices[j]}</button>`; //build the HTML to render buttons
+    buttonolEl.innerHTML += buttonAnswerEl;
+  }
+  buttonolEl.onclick = answerCheck; // drop a click listener on that whole thing and call the answerCheck function when clicked
 }
 
-function helloworld (ev) {
-  console.log ("hello here");
-  console.log (this.innerText);
+function countdownTimer() {
+  // quizOn = false;
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    counterEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      console.log("DONE");
+    }
+
+  }, 1000);
+
 }
+function answerCheck (event) {
+  // the click listener is applied to the whole DIV - so it's possible to receive clicks in the DIV but
+  // not on the button.  Fortunately, we can pull tagName from event.target and do stuff only if a 
+  // button was clicked.
 
-function render(questionNumber) {
-
-  // Clears existing data 
-  
-  questionCard.innerHTML = "";
-  btnCreate.innerHTML = "";
-  
-  // For loops to loop through all info in array
-  
-  for (var i = 0; i < questions.length; i++) {
-      // Appends question title only
-      var userQuestion = questions[questionIndex].question;
-      var allAvailableChoices = questions[questionIndex].choices;
-      questionCard.textContent = userQuestion;
+  if (event.target.tagName !== "BUTTON") {
+    return;  // we heard a click, but it wasn't on a BUTTON, so, peace out.
   }
   
-  // New for each for question choices
+  // event.target.textContent has the text of the answer.  Previously we set a glocal variable to correctAnswer
+  // so lets check them now and fire the approppriate steps.
   
-  allAvailableChoices.forEach(function (newItem) {
-      var listItem = document.createElement("button");
-      listItem.textContent = newItem;
-      questionCard.appendChild(btnCreate);
-      btnCreate.appendChild(listItem);
-      // listItem.addEventListener("click", (compare));
-  })
+  if (correctAnswer == event.target.textContent) {
+    console.log ("CORRECT and quizOn "+quizOn); // waits a second, then advances to the next question
+    } else {
+    console.log ("INCORRECT and quizOn "+quizOn) // adjust downward the timer, waits a second, advanced to the next question
+  }
+  setTimeout(() => {  console.log("waiting"); }, 1000); // here's our waiting a second
+  quizOn=true;
 
 }
-
-// render(0);
-
-// while (quanswers.lquestions[i].choices[j] {
-// // console.log ("there are "+questions.length+" elements in the questions array");
-// var chosenQuestionNum = Math.floor(Math.random() * questions.length);
-// // console.log ("i pick question number " + chosenQuestionNum);
-// // console.log ("there are "+ questions.length + " questions - now lets remove question number "+chosenQuestionNum);
-// var currentQuestion = questions[chosenQuestionNum];
-// // console.log ("current question is " + typeof (currentQuestion));
-// console.log ("current question is " + currentQuestion.question.toString());
-// questions.splice (chosenQuestionNum,1);
-// // console.log ("after removing index " + chosenQuestionNum + " we have " + questions.length + " questions left");
-// };
-
-// for (var i = 0; i < questions.length; i++) {
-
-
-// for (var i in questions) {
-//   currentquestion = questions[i].question;
-//   console.log("The question is " + questions[i].question);
-//   console.log("the correct answer is " + questions[i].correctAnswer);
-//   for (var j in questions[i].choices) {
-//     console.log("Option number " + j + " is " + questions[i].choices[j]);
-//   }
-// }
-
-// for (var i = 0; i < questions.length; i++) {
-//   currentquestion = questions[i].question;
-//   for (var j = 0; j < questions[i].choices.length; j++); {
-//     currentchoice = questions[i].choices[j];
-//     console.log ("q is "+currentquestion+" a  is "+currentchoice);
-//   }
-// }
